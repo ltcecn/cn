6c675ee7a4790028a703eedcbcb295ee4cd029a5
ltcecn <suuummiiit.programmer@gmail.com>
2024-10-23 10:02:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,30 @@ const server = net.createServer((socket) => {
 server.listen(5000, () => {
     console.log('Server is waiting for a connection on port 5000...');
 });
+
+
+
+
+
+// client.js
+const net = require('net');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const client = net.createConnection({ port: 5000, host:  '192.168.1.12' }, () => {
+    console.log('Connected to server!');
+    rl.question('Enter first number: ', (num1) => {
+        rl.question('Enter second number: ', (num2) => {
+            client.write(`${num1}\n${num2}\n`); // Send both numbers
+        });
+    });
+});
+
+client.on('data', (data) => {
+    console.log(data.toString());
+    client.end(); // Close the connection after receiving the result
+});
